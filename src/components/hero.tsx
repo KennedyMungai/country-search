b1229ecd2b3ce49d8d3e20235e79bd7f37cf35c7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-31 16:09:17 +0300

    Added some markup and logic to get search data from the backend

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,38 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 type Props = {}
 
 const Hero = () => {
-	return <div>Hero</div>
+	const [input, setInput] = useState<string>('')
+	const [searchResults, setSearchResults] = useState<
+		| {
+				results: string[]
+				duration: number
+		  }
+		| undefined
+	>()
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!input) return setSearchResults(undefined)
+
+			const res = await fetch(`/api/search?q=${input}`)
+		}
+
+		fetchData()
+	}, [input])
+
+	return (
+		<div>
+			<input
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+				type='text'
+			/>
+		</div>
+	)
 }
 
 export default Hero
