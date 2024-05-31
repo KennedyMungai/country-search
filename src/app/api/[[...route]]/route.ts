1d75f6b98eecb2328fc7f2eb5707b75810ad1d68
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-31 16:24:27 +0300

    Updated the address of the endpoint

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -5,6 +5,6 @@ export const runtime = 'edge'
 
 const app = new Hono()
 	.basePath('/api')
-	.get('/', async (c) => c.json({ message: 'Sup Bitches' }, 200))
+	.get('/search', async (c) => c.json({ message: 'Sup Bitches' }, 200))
 
 export const GET = handle(app)
