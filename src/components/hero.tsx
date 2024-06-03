9260f3c373e8eb3feb6496ee3b18f98a8d5a6c08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-06-03 11:29:58 +0300

    Added a parent animated div for the input component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -26,12 +26,14 @@ const Hero = () => {
 
 	return (
 		<div className='w-full h-full flex items-center justify-center bg-gradient-to-br from-violet-500 to-yellow-500'>
-			<input
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-				type='text'
-				className='bg-gray-800 p-2 text-white m-2 active:outline-none focus:outline-none'
-			/>
+			<div className='flex flex-col gap-6 items-center duration-500 animate animate-in fade-in-5 slide-in-from-bottom-2.5'>
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					type='text'
+					className='bg-gray-800 p-2 text-white m-2 active:outline-none focus:outline-none'
+				/>
+			</div>
 		</div>
 	)
 }
